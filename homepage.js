84207f03a2936e63fc4982426376acba13f85dc2
Cristian Lotero Aquino <miultimomail@hotmail.com>
2023-05-13 23:42:24 -0300

    style.css update

--- homepage.js
+++ homepage.js
@@ -2,5 +2,5 @@ let startButton = document.querySelector(".start");
 startButton.addEventListener("click" , goToRockPaperScissors);
 
 function goToRockPaperScissors() {
-    window.open("rockPaperScissors.html", "_self");
+    window.location.href ="rockPaperScissors.html";
 }
